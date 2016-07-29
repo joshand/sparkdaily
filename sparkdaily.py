c2a1de8171fb2dd6efdc7245707d1e04dabbc274
joshand <joshand@cisco.com>
2016-07-29 11:51:29 -0500

    Updated with conversation private API
    Made some changes to utilize the conversation API. Sending e-mails not enabled yet.

--- sparkdaily.py
+++ sparkdaily.py
@@ -5,25 +5,25 @@ import json
 import iso8601
 import smtplib
 import base64
+from dateutil import tz
+from unidecode import unidecode
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 token = config.token
 auth = "Bearer %s" % token
-room = config.roomid
+#room = config.roomid
 ignorelist = config.ignorelist
 date = (datetime.datetime.now() - datetime.timedelta(days=1)).date()
+headers = {
+    'authorization': auth,
+    'cache-control': "no-cache",
+    'content-type': 'application/json'
+    }
 
-
-def getMessages():
+def getMessages(room):
     url = "https://api.ciscospark.com/v1/messages"
     querystring = {"roomId": room}
-
-    headers = {
-        'authorization': auth,
-        'cache-control': "no-cache",
-        'content-type': 'application/json'
-        }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     messages = json.loads(response.content)
@@ -34,8 +34,6 @@ def getMessages():
 def todayMessage():
     messages = getMessages()
     todaymsg_list = []
-    if len(messages) == 0:
-        exit()
     for message in messages:
         msgdate = message[u'created']
         msgdate = iso8601.parse_date(msgdate).date()
@@ -44,18 +42,17 @@ def todayMessage():
             todaymsg_list.append(message)
     return todaymsg_list
 
-def createEmailBody(msg_list, roomId):
-    body = "Here is what you may have missed yesterday in %s, %s-%s-%s:\n" \
-           % (roomtitle, date.month, date.day, date.year)
+def createEmailBody(msg_list):
+    body = "Here is what you missed yesterday, %s-%s-%s:\n" % (date.month, date.day, date.year)
     for message in reversed(msg_list):
         #body = body , str(message[u'personEmail']), ": ", str(message[u'text']), "\n"
-        msgtime = str(iso8601.parse_date(message[u'created']).time().hour) + ":" +\
-                  str(iso8601.parse_date(message[u'created']).time().minute) + ":" + \
-                  str(iso8601.parse_date(message[u'created']).time().second)
+        #msgtime = str(iso8601.parse_date(message[u'created']).time().hour) + ":" +\
+        #          str(iso8601.parse_date(message[u'created']).time().minute) + ":" + \
+        #          str(iso8601.parse_date(message[u'created']).time().second)
+        msgtime = iso8601.parse_date(message[u'created']).strftime("%I:%M:%S%p")
         #body = body + "%s - %s:  \n" % (str(msgtime), str(message[u'personEmail']))
-        body = body + "%s - %s: %s \n" % (str(msgtime), getDisplayName(message[u'personId']),
+        body = body + "%s - %s: %s \n" % (str(msgtime), str(message[u'displayName']),
                                           message[u'text'])
-    body = body + "\n\nJoin the chat - https://web.ciscospark.com/#rooms/%s" % getRoomURL(roomId)
     body = body.encode('utf-8').strip()
     return body
 
@@ -63,11 +60,6 @@ def getUsers():
     url = "https://api.ciscospark.com/v1/memberships"
     querystring = {"roomId": room}
 
-    headers = {
-        'authorization': auth,
-        'cache-control': "no-cache",
-        'content-type': 'application/json'
-    }
     response = requests.request("GET", url, headers=headers, params=querystring)
     users = json.loads(response.content)
     users = users[u'items']
@@ -81,55 +73,161 @@ def getUsers():
 def getDisplayName(personId):
     url = "https://api.ciscospark.com/v1/people/" + personId
 
-    headers ={
-        'authorization': auth,
-        'cache-control': "no-cache",
-        'content-type': 'application/json'
-    }
     response = requests.request("GET", url, headers=headers)
     userinfo = json.loads(response.content)
     displayName = str(userinfo[u'displayName'])
 
     return displayName
 
-def getRoomTitle(roomId):
-    url = "https://api.ciscospark.com/v1/rooms/" + roomId
+def getRoomList():
+#	url = "https://api.ciscospark.com/v1/rooms"
+#	
+#	response = requests.request("GET", url, headers=headers)
+#	roominfo = json.loads(response.content)
+	roominfo = {u'items': [{u'created': u'2015-11-30T20:46:32.867Z', u'title': u'Thoughts in the Clouds', u'isLocked': False, u'lastActivity': u'2016-07-28T22:04:24.688Z', u'type': u'group', u'id': u'Y2lzY29zcGFyazovL3VzL1JPT00vNzA5MGNmMzAtOTdhMy0xMWU1LWIyNzAtZDM2ZWRmMzJlODMz'}]}
+	rooms = roominfo[u'items']
 
-    headers = {
-        'authorization': auth,
-        'cache-control': "no-cache",
-        'content-type': 'application/json'
-    }
-    response = requests.request("GET", url, headers=headers)
-    roominfo = json.loads(response.content)
-    print roominfo
-    title = str(roominfo[u'title'])
+	return rooms
 
-    return title
+def iterateRooms(rooms):
+	for room in rooms:
+		rid = str(room[u'id'])
+		rpkid = str(base64.b64decode(rid))
+		arr_rpkid = rpkid.split("/")
+		#print(arr_rpkid)
+		rpkid = arr_rpkid[-1]
+		rpkid = rpkid[:-1]								#in python3, there is an extra '
+		print(rid,rpkid)
+		participants = getParticipantList(rpkid)
+		iterateParticipants(participants,rpkid)
+	return 0
 
-def getRoomURL(roomId):
-    basedecode = base64.b64decode(roomId)
-    roomurl = basedecode.split('/')[-1]
-    #print roomurl
-    return roomurl
+def getParticipantList(roompkid):
+	url = "https://conv-a.wbx2.com/conversation/api/v1/conversations/" + roompkid + "?uuidEntryFormat=true&personRefresh=true&latestActivity=&participantAckFilter=all&activitiesLimit=30&ackFilter=noack"
+
+	response = requests.request("GET", url, headers=headers)
+	convoinfo = json.loads(response.content.decode('utf8'))
+	participants = convoinfo[u'participants'][u'items']
+
+	return participants
+
+def iterateParticipants(participants,roomid):
+	for participant in participants:
+		pt = participant[u'type']
+		if pt == "PERSON":
+			pe = participant[u'emailAddress']
+			pm = participant[u'roomProperties'][u'lastSeenActivityUUID']
+			pu = bytes("ciscospark://us/MESSAGE/" + pm, "ascii")
+			pi = base64.b64encode(pu).decode("ascii")
+			arrm = findMessageInRoom(roomid,pi)
+			
+			processParticipantAction(pi,pe,arrm)
+	
+	return 0
+
+def getDisplayName(userid):
+	url = "https://api.ciscospark.com/v1/people/" + userid
+
+	response = requests.request("GET", url, headers=headers)
+	userinfo = json.loads(response.content.decode('utf8'))
+	pd = userinfo[u'displayName']
+	
+	return pd
+
+def findMessageInRoom(roomid,messageid):
+	url = "https://api.ciscospark.com/v1/messages?roomId=" + roomid + "&max=100"
+
+	response = requests.request("GET", url, headers=headers)
+	msginfo = json.loads(response.content.decode('utf8'))
+	messages = msginfo[u'items']
+
+	ucache = {}
+	mcount = 0
+	mcache = {}
+	for message in messages:
+		mi = str(message[u'id'])
+		mt = unidecode(message[u'text'])
+		mc = str(message[u'created'])
+		mfi = str(message[u'personId'])
+		mfe = str(message[u'personEmail'])
+		mfd = mfe
+		if mfi in ucache:
+			mfd = ucache[mfi]
+		else:
+			mfd = str(getDisplayName(mfi))
+			ucache[mfi] = mfd
+
+		if mi == messageid:
+			return mcache
+		else:
+			mcache[mcount] = {"id": mi, "text": mt, "created": mc, "personEmail": mfe, "displayName": mfd}
+			mcount = mcount + 1
+			
+	return 0
+
+def processParticipantAction(partid, partemail, unreadmessages):
+	from_zone = tz.gettz('UTC')
+	to_zone = tz.gettz('America/Chicago')
+
+	print(partemail,"::",partid)
+	#print("::",unreadmessages,"::")
+	#print(type(unreadmessages))
+
+	if len(unreadmessages) > 0:
+		todaymsg_list = []
+		for messagenum in unreadmessages:
+			#print("Num=",messagenum)
+			#print("1")
+			message = unreadmessages[messagenum]
+			#print("2")
+			if 'created' in message:
+				msgdate = message.get('created')			#['created']
+				msgdate = str(iso8601.parse_date(msgdate))
+				arr_date = msgdate.split(".")
+				msgdate = arr_date[0]
+				#print(msgdate)
+				t = datetime.datetime.strptime(msgdate, '%Y-%m-%d %H:%M:%S')
+				t = t.replace(tzinfo=from_zone)
+				t = t.astimezone(to_zone)
+				message['created'] = str(t)
+				msgdate = t.date()
+	        	#print("2.4")
+	        	#print("Date2=",msgdate)
+	        	#print("2.5")
+				if date == msgdate:
+					todaymsg_list.append(message)
+
+        	#print("3")
+
+			#print(type(message))
+			#print("Msg=",message)
+			#if "created" in message:
+			#	print("found it")
+        	#print("Date2=",msgdate)
+        	#if date == msgdate:
+			#	todaymsg_list.append(message)
+		body = createEmailBody(todaymsg_list).decode("ascii")
+		print(body)
+
+	print("------------------------------------------\n")
 
 
-roomtitle = getRoomTitle(room)
+iterateRooms(getRoomList())
+#print(rooms)
 
+#sender = config.sender
+#server = config.server
+#server_port = config.server_port
 
-sender = config.sender
-server = config.server
-server_port = config.server_port
+#msg = MIMEMultipart()
+#body = MIMEText(str(createEmailBody(todayMessage())).strip())
 
-msg = MIMEMultipart()
-body = MIMEText(str(createEmailBody(todayMessage(), room)).strip())
+#msg['Subject'] = "Daily Summary"
+#msg['From'] = sender
+#msg['To'] = ", ".join(getUsers())
+#msg.attach(body)
 
-msg['Subject'] = "Daily Spark Summary for %s" % roomtitle
-msg['From'] = sender
-msg['To'] = ", ".join(getUsers())
-msg.attach(body)
+##print msg
 
-#print msg
-
-smtpObj = smtplib.SMTP(server, server_port)
-smtpObj.sendmail(msg["From"], msg["To"].split(","), msg.as_string())
+#smtpObj = smtplib.SMTP(server, server_port)
+#smtpObj.sendmail(msg["From"], msg["To"].split(","), msg.as_string())
